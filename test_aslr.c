e7448e381ff3d9e8deafa015c53daf31ed3ca686
duff <1059103811@qq.com>
2024-08-23 17:20:48 +0800

    Add notes

---
+++ test_aslr.c
@@ -0,0 +1,9 @@
+// A simple program to observe ASLR (Address Space Layout Randomization) in action.
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    int stack = 0;
+    printf("%p\n", &stack);
+    return 0;
+}
